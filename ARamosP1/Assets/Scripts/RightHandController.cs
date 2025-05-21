2e94e58196f42cc686f4b4494372156bf7b8474b
andrearamosz <andrea.ramosz@unisimon.edu.co>
2025-05-20 19:11:03 -0500

    Conf. SDS + Organización de la jerarquía
    SDS, XR Rig, Tunneling Vignette, Escena.

---
+++ ARamosP1/Assets/Scripts/RightHandController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RightHandController : MonoBehaviour
+{
+    //Estado
+    [SerializeField] private XRRayInteractor _XRRayInteractor_Grab;
+    [SerializeField] private XRRayInteractor _XRRayInteractor_Teleport;
+
+    [SerializeField] private InputActionReference _Joystick_North_Sector;
+
+    private void Awake()
+    {
+        _XRRayInteractor_Teleport.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        _Joystick_North_Sector.action.performed += PalancaArribaPresionada;
+        _Joystick_North_Sector.action.canceled += PalancaArribaLiberada;
+    }
+
+    private void OnDisable()
+    {
+        _Joystick_North_Sector.action.performed -= PalancaArribaPresionada;
+        _Joystick_North_Sector.action.canceled -= PalancaArribaLiberada;
+    }
+
+    private void PalancaArribaPresionada(InputAction.CallbackContext context)
+    {
+        _XRRayInteractor_Grab.enabled = false;
+        _XRRayInteractor_Teleport.enabled = true;
+    }
+
+    private void PalancaArribaLiberada(InputAction.CallbackContext context) => Invoke("PalancaArribaLiberada_Invoke", 0.01f);
+
+    private void PalancaArribaLiberada_Invoke()
+    {
+        _XRRayInteractor_Grab.enabled = true;
+        _XRRayInteractor_Teleport.enabled = false;
+    }
+}
